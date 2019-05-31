a5a7c7f7b85176dfeec45906f978a8cab8724d46
David Fischer <theupsider@gmx.de>
2019-05-31 17:47:50 +0200

    first commit

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.IO;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -12,17 +15,250 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.ComponentModel;
 
 namespace SongSync
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
+        private static Socket _clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private TcpClient client;
+        public StreamReader STR;
+        public StreamWriter STW;
+        public string recieve;
+        public String TextToSend;
+
+        private static BackgroundWorker backgroundWorker1 = new BackgroundWorker();
+        private static BackgroundWorker backgroundWorker2 = new BackgroundWorker();
+
         public MainWindow()
         {
+
             InitializeComponent();
+            backgroundWorker1.DoWork += new DoWorkEventHandler(backgroundWorker1_DoWork);
+            backgroundWorker2.DoWork += new DoWorkEventHandler(backgroundWorker2_DoWork);
+
+            IPAddress[] localIP = Dns.GetHostAddresses(Dns.GetHostName());
+
+            foreach (IPAddress address in localIP)
+            {
+                if (address.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    textBoxIPServer.Text = address.ToString();
+                    textBoxIPClient.Text = address.ToString();
+                }
+            }
+            //StartClient();
+            //LoopConnect();
+            //SendLoop();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (client.Connected)
+            {
+                try
+                {
+                    recieve = STR.ReadLine();
+                    this.chatBox.Dispatcher.Invoke(new Action(delegate ()
+                    {
+                        chatBox.AppendText("Other:" + recieve + "\n");
+                    }));
+                
+                    recieve = "";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (client.Connected)
+            {
+                STW.WriteLine(TextToSend);
+                this.chatBox.Dispatcher.Invoke(new Action(delegate ()
+                {
+                    chatBox.AppendText("Me:" + TextToSend + "\n");
+                }));
+            }
+            else
+            {
+                MessageBox.Show("Sending failed");
+            }
+            backgroundWorker2.CancelAsync();
+        }
+
+   
+
+        public static void StartClient()
+        {
+            byte[] bytes = new byte[1024];
+
+            try
+            {
+                // Connect to a Remote server  
+                // Get Host IP Address that is used to establish a connection  
+                // In this case, we get one IP address of localhost that is IP : 127.0.0.1  
+                // If a host has multiple addresses, you will get a list of addresses  
+                IPHostEntry host = Dns.GetHostEntry("localhost");
+                IPAddress ipAddress = host.AddressList[0];
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11000);
+
+                // Create a TCP/IP  socket.    
+                Socket sender = new Socket(ipAddress.AddressFamily,
+                    SocketType.Stream, ProtocolType.Tcp);
+
+                // Connect the socket to the remote endpoint. Catch any errors.    
+                try
+                {
+                    // Connect to Remote EndPoint  
+                    sender.Connect(remoteEP);
+
+                    Console.WriteLine("Socket connected to {0}",
+                        sender.RemoteEndPoint.ToString());
+
+                    // Encode the data string into a byte array.    
+                    byte[] msg = Encoding.ASCII.GetBytes("This is a test<EOF>");
+
+                    // Send the data through the socket.    
+                    int bytesSent = sender.Send(msg);
+
+                    // Receive the response from the remote device.    
+                    int bytesRec = sender.Receive(bytes);
+                    Console.WriteLine("Echoed test = {0}",
+                        Encoding.ASCII.GetString(bytes, 0, bytesRec));
+
+                    // Release the socket.    
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+
+                }
+                catch (ArgumentNullException ane)
+                {
+                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine("SocketException : {0}", se.ToString());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        private void SendLoop()
+        {
+            while (true)
+            {
+                Console.Write("Enter a request; ");
+                string req = "get time";
+                byte[] buffer = Encoding.ASCII.GetBytes(req);
+                _clientSocket.Send(buffer);
+
+                byte[] receivedBuff = new byte[1024];
+                int rec = _clientSocket.Receive(receivedBuff);
+                byte[] data = new byte[rec];
+                Array.Copy(receivedBuff, data, rec);
+                Console.WriteLine("Received: " + Encoding.ASCII.GetString(data));
+            }
+        }
+
+        private void LoopConnect()
+        {
+            int attempts = 0;
+
+            while (!_clientSocket.Connected)
+            {
+                try
+                {
+                    attempts++;
+                    _clientSocket.Connect(IPAddress.Loopback, 100);
+                }
+                catch (SocketException)
+                {
+                    Console.WriteLine("Connection Attempts: " + attempts);
+                }
+            }
+            Console.WriteLine("Connected");
+        }
+
+        private void ButtonPlay_Click(object sender, RoutedEventArgs e)
+        {
+                    TextToSend = "play";
+                    backgroundWorker2.RunWorkerAsync();            
+        }
+
+        public static async void callMethod()
+        {
+            Task task = SendPlay();
+            await task;
+        }
+
+        public static async Task SendPlay()
+        {
+            await Task.Run(() =>
+            {
+                Console.Write("Enter a request; ");
+                string req = "get time";
+                byte[] buffer = Encoding.ASCII.GetBytes(req);
+                _clientSocket.Send(buffer);
+
+                byte[] receivedBuff = new byte[1024];
+                int rec = _clientSocket.Receive(receivedBuff);
+                byte[] data = new byte[rec];
+                Array.Copy(receivedBuff, data, rec);
+                Console.WriteLine("Received: " + Encoding.ASCII.GetString(data));
+            });
+        }
+
+        private void ButtonServerStart_Click(object sender, RoutedEventArgs e)
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, int.Parse(textBoxPortServer.Text));
+            listener.Start();
+            client = listener.AcceptTcpClient();
+            STR = new StreamReader(client.GetStream());
+            STW = new StreamWriter(client.GetStream());
+            STW.AutoFlush = true;
+
+            backgroundWorker1.RunWorkerAsync();
+            backgroundWorker2.WorkerSupportsCancellation = true;
+        }
+
+        private void ButtonClientConnect_Click(object sender, RoutedEventArgs e)
+        {
+            client = new TcpClient();
+            IPEndPoint IpEnd = new IPEndPoint(IPAddress.Parse(textBoxIPClient.Text), int.Parse(textBoxPortClient.Text));
+
+            try
+            {
+                client.Connect(IpEnd);
+
+                if (client.Connected)
+                {
+                    chatBox.AppendText("Connected to server" + "\n");
+                    STW = new StreamWriter(client.GetStream());
+                    STR = new StreamReader(client.GetStream());
+                    STW.AutoFlush = true;
+                    backgroundWorker1.RunWorkerAsync();
+                    backgroundWorker2.WorkerSupportsCancellation = true;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
         }
     }
 }
